62b4840c6d713c6547054370fb7fea67d0074e71
Anwesh43 <anweshthecool0@gmail.com>
2019-11-22 10:37:48 +0530

    adding Animator class

--- linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
+++ linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
@@ -21,6 +21,7 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#1565C0")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 4f
+val delay : Long = 30
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -94,6 +95,34 @@ class LineRectBouncyView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
