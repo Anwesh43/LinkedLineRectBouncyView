ce4597d235cf5d2b9f72d154439c43fecde7a79f
Anwesh43 <anweshthecool0@gmail.com>
2019-11-22 10:17:14 +0530

    adding libraries and constatns

--- linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
+++ linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.linerectbouncyview
 /**
  * Created by anweshmishra on 22/11/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
