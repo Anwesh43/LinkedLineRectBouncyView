eb2127216a152adfa540a362f351fa9b534a55e4
Anwesh43 <anweshthecool0@gmail.com>
2019-11-22 10:31:51 +0530

    adding LineRectBouncyView class

--- linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
+++ linerectbouncyview/src/main/java/com/anwesh/uiprojects/linerectbouncyview/LineRectBouncyView.kt
@@ -60,3 +60,21 @@ fun Canvas.drawLRBNode(i : Int, scale : Float, paint : Paint) {
     drawLinesBouncyRect(size, scale, paint)
     restore()
 }
+
+class LineRectBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
